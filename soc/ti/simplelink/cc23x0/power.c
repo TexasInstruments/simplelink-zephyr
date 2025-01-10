3e9dff8fbb9288202478ca441136acc840a7df5b
Lars Thalian Morstad <l-morstad@ti.com>
2025-01-10 13:50:18 +0100

    soc: ti: simplelink: reverted to simplelink standby function
    pm_cc23x0_enter_standby is now similar to the enterStandby function for
    nortos in the simplelink 8.40.00.09 GA SDK.
    Enables systimer by default and removes rtc timer from OS timer choice.
    Includes power.c without needing to enable PM.
    Removes RTC timer implementation.
    
    Signed-off-by: Lars Thalian Morstad <l-morstad@ti.com>

--- soc/ti/simplelink/cc23x0/power.c
+++ soc/ti/simplelink/cc23x0/power.c
@@ -5,6 +5,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdbool.h>
+#include <string.h>
+
 #include <zephyr/kernel.h>
 #include <zephyr/init.h>
 #include <zephyr/sys/reboot.h>
@@ -37,168 +40,311 @@ const PowerCC23X0_Config PowerCC23X0_config = {
 
 #ifdef CONFIG_PM
 
-#ifndef CONFIG_CC23X0_RTC_TIMER
-
-#define MAX_SYSTIMER_DELTA 0xFFBFFFFFU
-#define RTC_TO_SYSTIM_TICKS 8U
-#define SYSTIM_CH_STEP      4U
-#define SYSTIM_CH(idx)      (SYSTIM_O_CH0CC + idx * SYSTIM_CH_STEP)
-#define SYSTIM_TO_RTC_SHIFT 3U
-#define SYSTIM_CH_CNT       5U
-#define RTC_CH_CNT          2U
-#define RTC_NEXT(val, now)  (((val - PowerCC23X0_WAKEDELAYSTANDBY) >> SYSTIM_TO_RTC_SHIFT) + now)
-
-#endif
-
 static void pm_cc23x0_enter_standby(void);
 static int power_initialize(void);
 extern int_fast16_t PowerCC23X0_notify(uint_fast16_t eventType);
 
-#ifndef CONFIG_CC23X0_RTC_TIMER
-static void pm_cc23x0_systim_standby_restore(void);
+/* Max number of ClockP ticks into the future supported by this ClockP
+ * implementation.
+ * Under the hood, ClockP uses the SysTimer whose events trigger immediately if
+ * the compare value is less than 2^22 systimer ticks in the past
+ * (4.194sec at 1us resolution). Therefore, the max number of SysTimer ticks you
+ * can schedule into the future is 2^32 - 2^22 - 1 ticks (~= 4290 sec at 1us
+ * resolution).
+ */
+#define MAX_SYSTIMER_DELTA 0xFFBFFFFFU
+
+/* The RTC has a time base of 8us and the SysTimer uses 1us or 250ns. The
+ * conversion factor assumes that the SysTimer values have been converted to 1us
+ * already.
+ */
+#define RTC_TO_SYSTIMER_TICKS 8U
+
+#define SYSTIMER_CHANNEL_COUNT 5U
 
 /* Global to stash the SysTimer timeouts while we enter standby */
-static uint32_t systim[SYSTIM_CH_CNT];
-static uint32_t rtc[RTC_CH_CNT];
-static uintptr_t key;
-static uint32_t systim_mask;
-static uint32_t rtc_mask;
+static uint32_t sysTimerTimeouts[SYSTIMER_CHANNEL_COUNT];
 
 /* Shift values to convert between the different resolutions of the SysTimer
  * channels. Channel 0 can technically support either 1us or 250ns. Until the
  * channel is actively used, we will hard-code it to 1us resolution to improve
  * runtime.
  */
-const uint8_t systim_offset[SYSTIM_CH_CNT] = {
-	0, /* 1us */
-	0, /* 1us */
-	2, /* 250ns -> 1us */
-	2, /* 250ns -> 1us */
-	2  /* 250ns -> 1us */
+static const uint8_t sysTimerResolutionShift[SYSTIMER_CHANNEL_COUNT] = {
+    0, /* 1us */
+    0, /* 1us */
+    2, /* 250ns -> 1us */
+    2, /* 250ns -> 1us */
+    2  /* 250ns -> 1us */
 };
-#endif
 
-#ifndef CONFIG_CC23X0_RTC_TIMER
-static void pm_cc23x0_systim_standby_restore(void)
-{
-	HWREG(RTC_BASE + RTC_O_ARMCLR) = RTC_ARMCLR_CH0_CLR;
-	HWREG(RTC_BASE + RTC_O_ICLR) = RTC_ICLR_EV0_CLR;
-
-	ULLSync();
-
-	HwiP_clearInterrupt(INT_CPUIRQ16);
-	HwiP_clearInterrupt(INT_CPUIRQ3);
-
-	HWREG(EVTSVT_BASE + EVTSVT_O_CPUIRQ16SEL) = EVTSVT_CPUIRQ16SEL_PUBID_SYSTIM0;
-	HWREG(EVTSVT_BASE + EVTSVT_O_CPUIRQ3SEL) = EVTSVT_CPUIRQ16SEL_PUBID_AON_RTC_COMB;
-
-	while (HWREG(SYSTIM_BASE + SYSTIM_O_STATUS) != SYSTIM_STATUS_VAL_RUN) {
-		;
-	}
-
-	for (uint8_t idx = 0; idx < SYSTIM_CH_CNT; idx++) {
-		if (systim_mask & (1 << idx)) {
-			HWREG(SYSTIM_BASE + SYSTIM_CH(idx)) = systim[idx];
-		}
-	}
-
-	HWREG(SYSTIM_BASE + SYSTIM_O_IMASK) = systim_mask;
-	HWREG(RTC_BASE + RTC_O_IMASK) = rtc_mask;
-
-	if (rtc_mask != 0) {
-		if (rtc_mask & 0x1) {
-			HWREG(RTC_BASE + RTC_O_CH0CC8U) = rtc[0];
-		}
-		if (rtc_mask & 0x2) {
-			HWREG(RTC_BASE + RTC_O_CH1CC8U) = rtc[1];
-		}
-	}
-
-	LRFDApplyClockDependencies();
-	PowerCC23X0_notify(PowerLPF3_AWAKE_STANDBY);
-	HwiP_restore(key);
-}
-#endif
-
+/* This function contains the logic required to decide if we enter standby or WFI
+ * It considers TI power constraints.
+ * This function is based on the PowerCC23X0_standbyPolicy() function for nortos
+ * from the TI SDK
+ */
 static void pm_cc23x0_enter_standby(void)
 {
-#ifndef CONFIG_CC23X0_RTC_TIMER
-	uint32_t rtc_now = 0;
-	uint32_t systim_now = 0;
-	uint32_t systim_next = MAX_SYSTIMER_DELTA;
-	uint32_t systim_delta = 0;
-	uint32_t rtc_delta = 0;
+    uint32_t constraints;
+    uint32_t sysTimerDelta;
+    uint32_t sysTimerIMASK;
+    uint32_t sysTimerARMSET;
+    uint32_t sysTimerLoopDelta;
+    uint32_t sysTimerCurrTime;
+    uint8_t sysTimerIndex;
+    uint32_t rtcCurrTime;
+    uintptr_t key;
+    bool standbyAllowed;
+    bool idleAllowed;
+    bool fltSettled;
+    bool lfTick;
 
-	key = HwiP_disable();
+    key = HwiP_disable();
 
-	uint32_t constraints = Power_getConstraintMask();
-	bool standby = (constraints & (1 << PowerLPF3_DISALLOW_STANDBY)) == 0;
-	bool idle = (constraints & (1 << PowerLPF3_DISALLOW_IDLE)) == 0;
+    /* Check state of constraints */
+    constraints    = Power_getConstraintMask();
+    standbyAllowed = (constraints & (1 << PowerLPF3_DISALLOW_STANDBY)) == 0;
+    idleAllowed    = (constraints & (1 << PowerLPF3_DISALLOW_IDLE)) == 0;
 
-	if (standby && (HWREG(CKMD_BASE + CKMD_O_LFCLKSEL) & CKMD_LFCLKSEL_MAIN_LFOSC) &&
-	    !(HWREG(CKMD_BASE + CKMD_O_LFCLKSTAT) & CKMD_LFCLKSTAT_FLTSETTLED_M)) {
-		standby = false;
-		idle = false;
-	}
+    /* If we are using LFOSC, we need to wait for the LFINC filter to settle
+     * before entering standby. We also cannot enter idle instead of standby
+     * because otherwise we could end up waiting for the next standby wakeup
+     * signal from the RTC or another wakeup source while we are still in idle.
+     * That could be a very long time.
+     * But if standby is currently disallowed from the constraints, that means
+     * we do want to enter idle since something set that constraint and will
+     * lift it again.
+     * Additionally, it has been observed a brief period of ~15us occurring
+     * ~130us after starting HFXT where FLTSETTLED pulses high. If the idle loop
+     * attempts to enter standby while FLTSETTLED pulses high, it may enter
+     * standby before the filter is truly settled. To prevent this, we need to
+     * wait until the next LFTICK after HFXTGOOD is set before going to sleep.
+     * This is achieved by checking against LFTICK before entering standby,
+     * which is cleared once AMPSETTLED is set (which occurs after HFXTGOOD is
+     * set).
+     */
+    if ((HWREG(CKMD_BASE + CKMD_O_LFCLKSEL) & CKMD_LFCLKSEL_MAIN_M) == CKMD_LFCLKSEL_MAIN_LFOSC)
+    {
+        if (standbyAllowed)
+        {
+            fltSettled = ((HWREG(CKMD_BASE + CKMD_O_LFCLKSTAT) & CKMD_LFCLKSTAT_FLTSETTLED_M) ==
+                           CKMD_LFCLKSTAT_FLTSETTLED);
+            lfTick     = ((HWREG(CKMD_BASE + CKMD_O_RIS) & CKMD_RIS_LFTICK_M) == CKMD_RIS_LFTICK);
+            if (!fltSettled || !lfTick)
+            {
+                standbyAllowed = false;
+                idleAllowed    = false;
+            }
+        }
+    }
 
-	if (standby) {
-		systim_mask = HWREG(SYSTIM_BASE + SYSTIM_O_IMASK);
-		if (systim_mask != 0) {
-			systim_next = 0xFFFFFFFF;
-			systim_now = HWREG(SYSTIM_BASE + SYSTIM_O_TIME1U);
-			for (uint8_t idx = 0; idx < SYSTIM_CH_CNT; idx++) {
-				if (systim_mask & (1 << idx)) {
-					systim[idx] = HWREG(SYSTIM_BASE + SYSTIM_CH(idx));
-					systim_delta = systim[idx];
-					systim_delta -= systim_now << systim_offset[idx];
+    /* Do quick check to see if only WFI allowed; if yes, do it now. */
+    if (standbyAllowed)
+    {
+        /* If we are allowed to enter standby, check whether the next timeout is
+         * far enough away for it to make sense.
+         */
 
-					if (systim_delta > MAX_SYSTIMER_DELTA) {
-						systim_delta = 0;
-					}
+        /* Save SysTimer IMASK to restore afterwards */
+        sysTimerIMASK = HWREG(SYSTIM_BASE + SYSTIM_O_IMASK);
 
-					systim_delta = systim_delta >> systim_offset[idx];
-					systim_next = MIN(systim_next, systim_delta);
-				}
-			}
-		} else {
-			systim_next = MAX_SYSTIMER_DELTA;
-		}
+        /* Get current armed status amongst all SysTimer channels */
+        sysTimerARMSET = HWREG(SYSTIM_BASE + SYSTIM_O_ARMSET);
 
-		rtc_mask = HWREG(RTC_BASE + RTC_O_IMASK);
-		if (rtc_mask != 0) {
-			rtc_now = HWREG(RTC_BASE + RTC_O_TIME8U);
-			if (rtc_mask & 0x1) {
-				rtc[0] = HWREG(RTC_BASE + RTC_O_CH0CC8U);
-				rtc_delta = (rtc[0] - rtc_now) << 3;
-				systim_next = MIN(systim_next, rtc_delta);
-			}
-			if (rtc_mask & 0x2) {
-				rtc[1] = HWREG(RTC_BASE + RTC_O_CH1CC8U);
-				rtc_delta = (rtc[1] - rtc_now) << 3;
-				systim_next = MIN(systim_next, rtc_delta);
-			}
-		}
+        /* Get current time in 1us resolution */
+        sysTimerCurrTime = HWREG(SYSTIM_BASE + SYSTIM_O_TIME1U);
 
-		if (systim_next > PowerCC23X0_TOTALTIMESTANDBY) {
-			HWREG(EVTSVT_BASE + EVTSVT_O_CPUIRQ3SEL) = 0;
-			HWREG(EVTSVT_BASE + EVTSVT_O_CPUIRQ16SEL) =
-			      EVTSVT_CPUIRQ16SEL_PUBID_AON_RTC_COMB;
-			HwiP_clearInterrupt(INT_CPUIRQ16);
-			rtc_now = HWREG(RTC_BASE + RTC_O_TIME8U);
-			HWREG(RTC_BASE + RTC_O_CH0CC8U) = RTC_NEXT(systim_next, rtc_now);
-#endif
+        /* Get current time in 8us resolution */
+        rtcCurrTime = HWREG(RTC_BASE + RTC_O_TIME8U);
+
+        /* We only want to check the SysTimer channels if at least one of them
+         * is active. It may be that no one is using ClockP or RCL in this
+         * application or they have not been initialised yet.
+         */
+        if (sysTimerIMASK != 0)
+        {
+            /* Set initial SysTimer delta to max possible value. It needs to be
+             * this large since we will shrink it down to the soonest timeout with
+             * Math_MIN() comparisons
+             */
+            sysTimerDelta = 0xFFFFFFFF;
+
+            /* Loop over all SysTimer channels and compute the soonest timeout.
+             * Since the channels have different time bases (1us vs 250ns),
+             * we need to shift all of that to a 1us time base to compare them.
+             * If no channel is active, we will use the max timeout value
+             * supported by the SysTimer.
+             */
+            for (sysTimerIndex = 0; sysTimerIndex < SYSTIMER_CHANNEL_COUNT; sysTimerIndex++)
+            {
+                if (sysTimerIMASK & (1 << sysTimerIndex))
+                {
+                    /* Stash SysTimer channel compare value. Read CHnCCSR to
+                     * avoid clearing any pending events as side effect of
+                     * reading CHnCC.
+                     */
+                    sysTimerTimeouts[sysTimerIndex] = HWREG(SYSTIM_BASE + SYSTIM_O_CH0CCSR + sysTimerIndex * sizeof(uint32_t));
+
+                    /* Store current channel timeout in native channel
+                     * resolution
+                     */
+                    sysTimerLoopDelta = sysTimerTimeouts[sysTimerIndex];
+
+                    /* Convert current time from 1us to native resolution and
+                     * subtract from timeout to get delta in in native channel
+                     * resolution.
+                     * We compute the delta in the native resolution
+                     * to correctly handle wrapping and underflow at the 32-bit
+                     * boundary.
+                     * To simplify code paths and SRAM, we shift up the 1us
+                     * resolution time stamp instead of reading out and keeping
+                     * track of the 250ns time stamp and associating that with
+                     * channels 2 to 4. The loss of resolution for wakeup is not
+                     * material as we wake up sufficiently early to handle
+                     * timing jitter in the wakeup duration.
+                     */
+                    sysTimerLoopDelta -= sysTimerCurrTime << sysTimerResolutionShift[sysTimerIndex];
+
+                    /* If sysTimerDelta is larger than MAX_SYSTIMER_DELTA, the
+                     * compare event happened in the past and we need to abort
+                     * entering standby to handle the timeout instead of waiting
+                     * a really long time.
+                     */
+                    if (sysTimerLoopDelta > MAX_SYSTIMER_DELTA)
+                    {
+                        sysTimerLoopDelta = 0;
+                    }
+
+                    /* Convert delta to 1us resolution */
+                    sysTimerLoopDelta = sysTimerLoopDelta >> sysTimerResolutionShift[sysTimerIndex];
+
+                    /* Update the smallest SysTimer delta */
+                    sysTimerDelta = Math_MIN(sysTimerDelta, sysTimerLoopDelta);
+                }
+            }
+        }
+        else
+        {
+            /* None of the SysTimer channels are active. Use the maximum
+             * SysTimer delta instead. That lets us sleep for at least this
+             * long if the OS timeout is even longer.
+             */
+            sysTimerDelta = MAX_SYSTIMER_DELTA;
+        }
+
+        /* Check sysTimerDelta time vs STANDBY latency */
+        if (sysTimerDelta > PowerCC23X0_TOTALTIMESTANDBY)
+        {
+            /* Switch EVTSVT_O_CPUIRQ16SEL in eventfabric to RTC
+             * Since the CC23X0 only has limited interrupt lines, we need
+             * to switch the interrupt line from SysTimer to RTC in the
+             * event fabric.
+             * The triggered interrupt will wake up the device with
+             * interrupts disabled. We can consume that interrupt event
+             * without vectoring to the ISR and then change the event fabric
+             * signal back to the SysTimer.
+             * Thus, there is no need to swap out the actual interrupt
+             * function of the clockHwi.
+             */
+            HWREG(EVTSVT_BASE + EVTSVT_O_CPUIRQ16SEL) = EVTSVT_CPUIRQ16SEL_PUBID_AON_RTC_COMB;
+
+            /* Clear interrupt in case it triggered since we disabled interrupts */
+            HwiP_clearInterrupt(INT_CPUIRQ16);
+
+            /* Ensure the device wakes up early enough to reinitialise the
+             * HW and take care of housekeeping.
+             */
+            sysTimerDelta -= PowerCC23X0_WAKEDELAYSTANDBY;
+
+            /* The SysTimer has a time base of 1us while the RTC uses 8us.
+             * Divide by 8 to convert from SysTimer to RTC time base
+             */
+            sysTimerDelta /= RTC_TO_SYSTIMER_TICKS;
+
+            /* RTC channel 0 compare is automatically armed upon writing the
+             * compare value. It will automatically be disarmed when it
+             * triggers.
+             */
+            HWREG(RTC_BASE + RTC_O_CH0CC8U) = rtcCurrTime + sysTimerDelta;
+
 			Power_sleep(PowerLPF3_STANDBY);
-#ifndef CONFIG_CC23X0_RTC_TIMER
-			pm_cc23x0_systim_standby_restore();
-		} else if (idle) {
-			__WFI();
-		}
-	} else if (idle) {
-		__WFI();
-	}
 
-	HwiP_restore(key);
-#endif
+            /* Disarm RTC compare event in case we woke up from a GPIO or BATMON
+             * event. If the RTC times out after clearing RIS and the pending
+             * NVIC bit but before we swap event fabric subscribers for the
+             * shared interrupt line, we will be left with a pending interrupt
+             * in the NVIC that the ClockP callback may not gracefully handle
+             * since it did not cause it itself.
+             */
+            HWREG(RTC_BASE + RTC_O_ARMCLR) = RTC_ARMCLR_CH0_CLR;
+
+            /* Clear the RTC wakeup event */
+            HWREG(RTC_BASE + RTC_O_ICLR) = RTC_ICLR_EV0_CLR;
+
+            /* Explicitly read back from ULL domain to guarantee clearing RIS
+             * takes effect before clearing the pending NVIC interrupt to avoid
+             * the NVIC re-asserting on a set RIS.
+             */
+            ULLSync();
+
+            /* Clear any pending interrupt in the NVIC */
+            HwiP_clearInterrupt(INT_CPUIRQ16);
+
+            /* Switch EVTSVT_O_CPUIRQ16SEL in eventfabric back to SysTimer */
+            HWREG(EVTSVT_BASE + EVTSVT_O_CPUIRQ16SEL) = EVTSVT_CPUIRQ16SEL_PUBID_SYSTIM0;
+
+            /* When waking up from standby, the SysTimer may not have
+             * synchronised with the RTC by now. Wait for SysTimer
+             * synchronisation with the RTC to complete. This should not take
+             * more than one LFCLK period.
+             *
+             * We need to wait both for RUN to be set and SYNCUP to go low. Any
+             * other register state will cause undefined behaviour.
+             */
+            while (HWREG(SYSTIM_BASE + SYSTIM_O_STATUS) != SYSTIM_STATUS_VAL_RUN) {}
+
+            /* Restore SysTimer timeouts */
+            memcpy((void *)(SYSTIM_BASE + SYSTIM_O_CH0CCSR), sysTimerTimeouts, sizeof(sysTimerTimeouts));
+
+            /* Restore SysTimer armed state. This will rearm all previously
+             * armed timeouts restored above and cause any that occurred in the
+             * past to trigger immediately.
+             */
+            HWREG(SYSTIM_BASE + SYSTIM_O_ARMSET) = sysTimerARMSET;
+
+            /* Restore SysTimer IMASK */
+            HWREG(SYSTIM_BASE + SYSTIM_O_IMASK) = sysTimerIMASK;
+
+            /* Re-configure LRFD clocks */
+            LRFDApplyClockDependencies();
+
+            /* Signal clients registered for standby wakeup notification;
+             * this should be used to initialize any timing critical or IO
+             * dependent hardware.
+             * The callback needs to go out after the SysTimer is restored
+             * such that notifications can invoke RCL and ClockP APIs if needed.
+             */
+            PowerCC23X0_notify(PowerLPF3_AWAKE_STANDBY);
+            HwiP_restore(key);
+		}
+        else if (idleAllowed)
+        {
+            /* If we would be allowed to enter standby but there is not enough
+             * time for it to make sense from an overhead perspective, enter idle
+             * instead.
+             */
+            __WFI();
+        }
+    }
+    else if (idleAllowed)
+    {
+        /* We are not allowed to enter standby.
+         * Enter idle instead if it is allowed.
+         */
+        __WFI();
+    }
+
+    HwiP_restore(key);
 }
 
 void pm_state_set(enum pm_state state, uint8_t substate_id)
