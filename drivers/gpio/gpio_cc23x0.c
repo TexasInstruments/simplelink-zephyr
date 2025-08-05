36a6af10db3d4a927617e0a9fa2fbdb8c44f34db
Eirik Runshaug <e-runshaug@ti.com>
2025-08-05 10:46:48 +0200

    drivers: gpio: remove CLKCTL function calls in pm_action
    remove CLKCTLEnable and CLKCTLDisable since clock settings are
    retained.
    
    Signed-off-by: Eirik Runshaug <e-runshaug@ti.com>

--- drivers/gpio/gpio_cc23x0.c
+++ drivers/gpio/gpio_cc23x0.c
@@ -262,12 +262,13 @@ static void gpio_cc23x0_isr(const struct device *dev)
 #ifdef CONFIG_PM_DEVICE
 static int gpio_cc23x0_pm_action(const struct device *dev, enum pm_device_action action)
 {
+	/* No action is done when suspending or resuming the GPIO module,
+	 * since both clock control and the GPIO peripheral have memory retention.
+	 */
 	switch (action) {
 	case PM_DEVICE_ACTION_RESUME:
-		CLKCTLEnable(CLKCTL_BASE, CLKCTL_GPIO);
 		break;
 	case PM_DEVICE_ACTION_SUSPEND:
-		CLKCTLDisable(CLKCTL_BASE, CLKCTL_GPIO);
 		break;
 	default:
 		return -ENOTSUP;
